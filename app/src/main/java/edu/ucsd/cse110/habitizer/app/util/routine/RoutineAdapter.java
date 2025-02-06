e1bc07ea94badd0b4c86c3759e68f10c3e805ff4
Kaz <80447192+miyatakazuya@users.noreply.github.com>
2025-02-05 22:54:15 -0800

    Added Fragment Routine, and fragment class

---
+++ app/src/main/java/edu/ucsd/cse110/habitizer/app/util/routine/RoutineAdapter.java
@@ -0,0 +1,11 @@
+package edu.ucsd.cse110.habitizer.app.util.routine;
+
+import android.content.Context;
+
+//TODO: Adapter needs to extend ArrayAdapter<T>, but task class not in this branch
+public class RoutineAdapter {
+
+    public RoutineAdapter(Context context) {
+
+    }
+}
