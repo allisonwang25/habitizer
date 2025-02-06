7cf23a8f52a1bbe281a5ed01a3fe3fac2129ee34
Christopher Lee <chrisdenglee@gmail.com>
2025-02-05 21:46:23 -0800

    add high level application file

---
+++ app/src/main/java/edu/ucsd/cse110/habitizer/app/HabitizerApplication.java
@@ -0,0 +1,10 @@
+package edu.ucsd.cse110.habitizer.app;
+
+import android.app.Application;
+
+public class HabitizerApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
