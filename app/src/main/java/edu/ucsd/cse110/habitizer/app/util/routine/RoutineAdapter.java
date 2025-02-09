89a415634d1c0dc413bc0afc8ca1b2a4a632dfca
Kaz <80447192+miyatakazuya@users.noreply.github.com>
2025-02-08 21:08:15 -0800

    readded files

---
+++ app/src/main/java/edu/ucsd/cse110/habitizer/app/util/routine/RoutineAdapter.java
@@ -0,0 +1,10 @@
+package edu.ucsd.cse110.habitizer.app.util.routine;
+
+import android.content.Context;
+
+//TODO: Adapter needs to extend ArrayAdapter<T>, but task class not in this branch yet
+public class RoutineAdapter {
+    public RoutineAdapter(Context context) {
+
+    }
+}
