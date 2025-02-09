f5b5ed6141c37e7f22542e476d62746530da2774
Kaz <80447192+miyatakazuya@users.noreply.github.com>
2025-02-08 21:08:15 -0800

    readded files

---
+++ app/src/main/java/edu/ucsd/cse110/habitizer/app/util/routine/RoutineFragment.java
@@ -0,0 +1,60 @@
+package edu.ucsd.cse110.habitizer.app.util.routine;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.ucsd.cse110.habitizer.app.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link RoutineFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class RoutineFragment extends Fragment {
+
+
+    public RoutineFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment RoutineFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static RoutineFragment newInstance(String param1, String param2) {
+        RoutineFragment fragment = new RoutineFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // TODO: Initialize Model
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(
+        @NonNull LayoutInflater inflater,
+        @Nullable ViewGroup container,
+        @Nullable Bundle savedInstanceState
+    ) {
+        // TODO: change this
+        return inflater.inflate(R.layout.fragment_routine, container, false);
+    }
+}
