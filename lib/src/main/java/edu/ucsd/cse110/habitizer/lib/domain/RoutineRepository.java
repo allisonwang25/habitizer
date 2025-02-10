94c28f51cbb721cb69f0cdbe270e9a192ee29f5e
allisonwang25 <allisonwang25@gmail.com>
2025-02-10 13:29:55 -0800

    viewmodel updates

---
+++ lib/src/main/java/edu/ucsd/cse110/habitizer/lib/domain/RoutineRepository.java
@@ -0,0 +1,30 @@
+package edu.ucsd.cse110.habitizer.lib.domain;
+
+import java.util.List;
+
+import edu.ucsd.cse110.habitizer.lib.data.InMemoryDataSource;
+import edu.ucsd.cse110.observables.PlainMutableSubject;
+
+public class RoutineRepository {
+    private final InMemoryDataSource dataSource;
+
+    public RoutineRepository(InMemoryDataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public Integer count() {
+        return dataSource.getRoutines().size();
+    }
+
+    public PlainMutableSubject<Routine> find(int id) {
+        return (PlainMutableSubject<Routine>) dataSource.getRoutineSubject(id);
+    }
+
+    public PlainMutableSubject<List<Routine>> findAll() {
+        return (PlainMutableSubject<List<Routine>>) dataSource.getAllRoutinesSubject();
+    }
+
+    public void save(Routine routine) {
+        dataSource.putRoutine(routine);
+    }
+}
