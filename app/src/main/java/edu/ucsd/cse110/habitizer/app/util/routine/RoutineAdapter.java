3ecc9935955dd581564f978c1a124e26035b5504
Christopher Lee <chrisdenglee@gmail.com>
2025-02-12 21:29:14 -0800

    add conditional to add strikethrough ui if checked-off

--- app/src/main/java/edu/ucsd/cse110/habitizer/app/util/routine/RoutineAdapter.java
+++ app/src/main/java/edu/ucsd/cse110/habitizer/app/util/routine/RoutineAdapter.java
@@ -35,7 +35,6 @@ public class RoutineAdapter extends ArrayAdapter<Task> {
         }
 
         binding.taskTitle.setText(task.getName());
-
         binding.taskCardLayout.setOnClickListener(v -> {
             task.checkOff();
 
@@ -43,10 +42,9 @@ public class RoutineAdapter extends ArrayAdapter<Task> {
             // https://stackoverflow.com/questions/3881553/is-there-an-easy-way-to-strike-through-text-in-an-app-widget/6739637#6739637
             // 2025 02 12
             // Took the method of setting flags to set strikethrough
-            binding.taskTitle.setPaintFlags(binding.taskTitle.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
             binding.elapsedTime.setText(String.format(String.valueOf(task.getTimeElapsed()) + " Minutes Elapsed"));
+            binding.taskTitle.setPaintFlags(binding.taskTitle.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
         });
-
         return binding.getRoot();
     }
 
