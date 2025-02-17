af3c9aa3c16c831b3b9677b34ce9a69559459610
Kevin Lu <kevinlu@acmucsd.org>
2025-02-16 22:53:55 -0800

    fix: merge conflict with master

--- app/src/main/java/edu/ucsd/cse110/habitizer/app/util/routine/MorningEditRoutineFragment.java
+++ app/src/main/java/edu/ucsd/cse110/habitizer/app/util/routine/MorningEditRoutineFragment.java
@@ -61,6 +61,7 @@ public class MorningEditRoutineFragment extends Fragment {
         });
 
         activityModel.getOrderedTasks().observe(tasks -> {
+            Log.d("test", "hi");
             if (tasks == null) return;
             adapter.clear();
             adapter.addAll(activityModel.getOrderedRoutines().getValue().get(0).getTasks());
