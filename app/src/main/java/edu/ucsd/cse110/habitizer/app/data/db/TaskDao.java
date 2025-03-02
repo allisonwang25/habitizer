740495f04c70543da0930b78c4192aa422bc3e7c
Kaz <80447192+miyatakazuya@users.noreply.github.com>
2025-03-02 13:42:59 -0800

    added simple dao queries

--- app/src/main/java/edu/ucsd/cse110/habitizer/app/data/db/TaskDao.java
+++ app/src/main/java/edu/ucsd/cse110/habitizer/app/data/db/TaskDao.java
@@ -1,7 +1,27 @@
 package edu.ucsd.cse110.habitizer.app.data.db;
 
 import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
 
 @Dao
 public interface TaskDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    Long insert(TaskEntity task);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    Long insert(List<TaskEntity> tasks);
+
+    @Query("DELETE FROM Task WHERE tid = :tid")
+    void remove(int tid);
+
+    @Query("SELECT * FROM Task WHERE tid = :tid")
+    TaskEntity find(int tid);
+
+    @Query("SELECT count(*) FROM Task")
+    int count();
+
 }
