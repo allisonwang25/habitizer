30b9cdfd746cba10b0c49e970ba8af119ad4f6ed
Kaz <80447192+miyatakazuya@users.noreply.github.com>
2025-03-03 14:08:55 -0800

    comment taskdatabase, waiting on Routine

--- app/src/main/java/edu/ucsd/cse110/habitizer/app/data/db/TaskDatabase.java
+++ app/src/main/java/edu/ucsd/cse110/habitizer/app/data/db/TaskDatabase.java
@@ -1,9 +1,9 @@
-package edu.ucsd.cse110.habitizer.app.data.db;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {TaskEntity.class}, version = 1)
-public abstract class TaskDatabase extends RoomDatabase {
-    public abstract TaskDao taskDao();
-}
+//package edu.ucsd.cse110.habitizer.app.data.db;
+//
+//import androidx.room.Database;
+//import androidx.room.RoomDatabase;
+//
+//@Database(entities = {TaskEntity.class}, version = 1)
+//public abstract class TaskDatabase extends RoomDatabase {
+//    public abstract TaskDao taskDao();
+//}
