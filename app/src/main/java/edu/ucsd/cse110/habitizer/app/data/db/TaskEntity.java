5fdc9b1da88ba98c1859e39f7b0df46574f05055
Kaz <80447192+miyatakazuya@users.noreply.github.com>
2025-03-11 21:54:39 -0700

    Merge

--- app/src/main/java/edu/ucsd/cse110/habitizer/app/data/db/TaskEntity.java
+++ app/src/main/java/edu/ucsd/cse110/habitizer/app/data/db/TaskEntity.java
@@ -1,47 +1,8 @@
 package edu.ucsd.cse110.habitizer.app.data.db;
 
-import androidx.annotation.NonNull;
-import androidx.room.ColumnInfo;
 import androidx.room.Entity;
-import androidx.room.ForeignKey;
-import androidx.room.PrimaryKey;
 
-import edu.ucsd.cse110.habitizer.lib.domain.Task;
-
-@Entity(
-    tableName = "Task",
-    foreignKeys = @ForeignKey(
-        entity = RoutineEntity.class,
-        parentColumns = "rid",
-        childColumns = "rid",
-        onDelete = ForeignKey.CASCADE
-    )
-)
+@Entity(tableName = "Task")
 public class TaskEntity {
-    @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "tid")
-    public Integer tid = null;
-
-    @ColumnInfo(name = "rid")
-    public Integer rid = null;
-
-    @ColumnInfo(name = "isCheckedOff")
-    public Boolean isCheckedOff = null;
-
-    TaskEntity(@NonNull Integer tid, @NonNull Integer rid, @NonNull Boolean isCheckedOff) {
-        this.tid = tid;
-        this.rid = rid;
-        this.isCheckedOff = isCheckedOff;
-    }
-
-    //  TODO: Constructor depends on setting rid, which doesn't exist in the Task Class as of now
-//    public static TaskEntity fromTask(@NonNull Task task) {
-//        var task_card = new TaskEntity(task.getId())
-//    }
-
-    // TODO: Same deal, incompatible constructors
-//    public @NonNull Task toTask() {
-////        return new Task(tid, )
-//    }
 
 }
