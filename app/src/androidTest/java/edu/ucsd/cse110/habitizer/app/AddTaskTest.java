583eb372b92d30dabb42ae201db3f5a4fae084a9
Christopher Lee <87111319+AskewParity@users.noreply.github.com>
2025-03-11 22:49:04 -0700

    Task/us18-1/add things (#221)
    * check edit button goes to correct view
    
    * AddTask Checked
    
    * tests adding routines
    
    * Merge pull request #212 from CSE-110-Winter-2025/task/us15-1-timer-pause (#220)
    
    * add ui buttons
    
    * hook up UI buttons to timer pause/resume
    
    * fixed timer still running when paused issue
    
    * fix bug where pause time does not work on first click
    
    * fix stop timer/advance time mocking functionality
    
    * edit ui to not round up routine time
    
    * update comment to be 100ms instead of 1s
    
    * check 'persistence' of added task
    
    * feat: changed advance time button to only advance time by 15 seconds (#222)
    
    Co-authored-by: Kevin Lu <kevinlu@acmucsd.org>
    
    * Story/us17/5 second increment on tasks (#207)
    
    * feat: made task timer be in increments of 5 seconds if under a min
    
    * chore: update timer to round up when at 55-60 seconds
    
    * Story/us12 task ordering (#189)
    
    * create ui task ordering buttons
    
    * moved UI to task_card_edit
    
    * delete unnecessary whitespace
    
    * added sortOrder field to task
    
    * implementing sorting based on sortOrder
    
    * changing mTaskRepo + eTaskRepo to just taskRepo
    
    * hook up and create button to routine moving
    
    * refactored moveTaskUp + moveTaskDown, need to figure out how to not sort in routine methods
    
    * move all sorting to inmemorydatasource
    
    * fixed getId to getTid
    
    * create ui task ordering buttons
    
    * moved UI to task_card_edit
    
    * delete unnecessary whitespace
    
    * refactor back button UI
    
    * feat: refactored routine list to support any given number of fragments
    
    * update routine to not have taskSubject + fix inmemorydatasource tests
    
    * adding shiftsortorders stuff to removeTask + fix compile error in routineFragment
    
    * update task usage in task tests
    
    * feat: implemented curr task timer
    
    * refactored moveTaskUp + moveTaskDown, need to figure out how to not sort in routine methods
    
    * moved UI to task_card_edit
    
    * feat: refactored routine list to support any given number of fragments
    
    * adding shiftsortorders stuff to removeTask + fix compile error in routineFragment
    
    * deleting unneccesarry layouts
    
    * re-remove buttons from run view
    
    * refactored a lot of accessing of stuff from fragmnetrs directly in inmemdatsource + add feat complete on all tasks finished
    
    * delete unnecessary comments
    
    * Removing Debug statmenets
    
    * constraining task title size on the right to the upbutton
    
    ---------
    
    Co-authored-by: allisonwang25 <allisonwang25@gmail.com>
    Co-authored-by: Kevin Lu <kevinlu@acmucsd.org>
    Co-authored-by: allisonwang25 <87393358+allisonwang25@users.noreply.github.com>
    
    * Merge pull request #212 from CSE-110-Winter-2025/task/us15-1-timer-pause (#220)
    
    * add ui buttons
    
    * hook up UI buttons to timer pause/resume
    
    * fixed timer still running when paused issue
    
    * fix bug where pause time does not work on first click
    
    * fix stop timer/advance time mocking functionality
    
    * edit ui to not round up routine time
    
    * update comment to be 100ms instead of 1s
    
    * fix: round up on 5 second timer instead of rounding down
    
    ---------
    
    Co-authored-by: Kevin Lu <kevinlu@acmucsd.org>
    Co-authored-by: Christopher Lee <87111319+AskewParity@users.noreply.github.com>
    Co-authored-by: jliu312 <85335934+jliu312@users.noreply.github.com>
    Co-authored-by: allisonwang25 <allisonwang25@gmail.com>
    Co-authored-by: allisonwang25 <87393358+allisonwang25@users.noreply.github.com>
    
    * check to see added task is in run
    
    ---------
    
    Co-authored-by: jliu312 <85335934+jliu312@users.noreply.github.com>
    Co-authored-by: kevinlu021 <115970778+kevinlu021@users.noreply.github.com>
    Co-authored-by: Kevin Lu <kevinlu@acmucsd.org>
    Co-authored-by: allisonwang25 <allisonwang25@gmail.com>
    Co-authored-by: allisonwang25 <87393358+allisonwang25@users.noreply.github.com>

---
+++ app/src/androidTest/java/edu/ucsd/cse110/habitizer/app/AddTaskTest.java
@@ -0,0 +1,202 @@
+package edu.ucsd.cse110.habitizer.app;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.pressKey;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.hamcrest.Matchers.anything;
+
+import android.widget.ListView;
+
+@RunWith(AndroidJUnit4.class)
+public class AddTaskTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void testEditClickOpensEditFragment() {
+        onView(withId(R.id.routine_list_view)).check(matches(isDisplayed()));
+
+        onData(anything())
+                .inAdapterView(withId(R.id.routine))
+                .atPosition(0)                  // Morning Routine List?
+                .onChildView(withId(R.id.routine_edit_btn))
+                .perform(click());
+
+        onView(withId(R.id.fragment_edit_routine)).check(matches(isDisplayed()));
+    }
+
+    // Tests Adding a Task
+    @Test
+    public void testAddTask() {
+        onView(withId(R.id.routine_list_view)).check(matches(isDisplayed()));
+
+        onData(anything())
+                .inAdapterView(withId(R.id.routine))
+                .atPosition(0)                  // Morning Routine List?
+                .onChildView(withId(R.id.routine_edit_btn))
+                .perform(click());
+
+        onView(withId(R.id.fragment_edit_routine)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.add_task_button)).perform(click());
+
+        onView(withId(R.id.new_task_dialog)).check(matches(isDisplayed()));
+    }
+
+    // Tests Typing a Title into the Add Task Dialog box
+    @Test
+    public void testAddTaskTitle() {
+        onView(withId(R.id.routine_list_view)).check(matches(isDisplayed()));
+
+        onData(anything())
+                .inAdapterView(withId(R.id.routine))
+                .atPosition(0)                  // Morning Routine List?
+                .onChildView(withId(R.id.routine_edit_btn))
+                .perform(click());
+
+        onView(withId(R.id.fragment_edit_routine)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.add_task_button)).perform(click());
+
+        onView(withId(R.id.new_task_dialog)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.taskNameInput)).perform(typeText("Test Task"), closeSoftKeyboard());
+
+        onView(withId(R.id.taskNameInput)).check(matches(withText("Test Task")));
+    }
+
+    // Adds task, and check it
+    @Test
+    public void testAddTaskCheck() {
+        onView(withId(R.id.routine_list_view)).check(matches(isDisplayed()));
+
+        onData(anything())
+                .inAdapterView(withId(R.id.routine))
+                .atPosition(0)
+                .onChildView(withId(R.id.routine_edit_btn))
+                .perform(click());
+
+        onView(withId(R.id.fragment_edit_routine)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.add_task_button)).check(matches(isDisplayed())).perform(click());
+
+        onView(withId(R.id.new_task_dialog)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.taskNameInput)).perform(typeText("Test Task"), closeSoftKeyboard());
+
+        onView(withId(R.id.taskNameInput)).check(matches(withText("Test Task")));
+
+        onView(withText("Create")).perform(click());
+
+        onView(withId(R.id.fragment_edit_routine)).check(matches(isDisplayed()));
+
+        int lastPosition = getListViewSize(R.id.routine) - 1;
+
+        onData(anything())
+                .inAdapterView(withId(R.id.routine))
+                .atPosition(lastPosition)
+                .onChildView(withId(R.id.task_title))
+                .check(matches(withText("Test Task")));
+    }
+
+    @Test
+    public void testAddTaskPersistence() {
+        onView(withId(R.id.routine_list_view)).check(matches(isDisplayed()));
+
+        onData(anything())
+                .inAdapterView(withId(R.id.routine))
+                .atPosition(0)
+                .onChildView(withId(R.id.routine_edit_btn))
+                .perform(click());
+
+        onView(withId(R.id.fragment_edit_routine)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.add_task_button)).check(matches(isDisplayed())).perform(click());
+
+        onView(withId(R.id.new_task_dialog)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.taskNameInput)).perform(typeText("Test Task"), closeSoftKeyboard());
+
+        onView(withId(R.id.taskNameInput)).check(matches(withText("Test Task")));
+
+        onView(withText("Create")).perform(click());
+
+        onView(withId(R.id.fragment_edit_routine)).check(matches(isDisplayed()));
+
+        int lastPosition = getListViewSize(R.id.routine) - 1;
+
+        onData(anything())
+                .inAdapterView(withId(R.id.routine))
+                .atPosition(lastPosition)
+                .onChildView(withId(R.id.task_title))
+                .check(matches(withText("Test Task")));
+
+        onView(withId(R.id.back_button)).perform(click());
+
+        // duplicate of previous
+        onView(withId(R.id.routine_list_view)).check(matches(isDisplayed()));
+
+        onData(anything())
+                .inAdapterView(withId(R.id.routine))
+                .atPosition(0)
+                .onChildView(withId(R.id.routine_edit_btn))
+                .perform(click());
+
+        onView(withId(R.id.fragment_edit_routine)).check(matches(isDisplayed()));
+
+        lastPosition = getListViewSize(R.id.routine) - 1;
+
+        onData(anything())
+                .inAdapterView(withId(R.id.routine))
+                .atPosition(lastPosition)
+                .onChildView(withId(R.id.task_title))
+                .check(matches(withText("Test Task")));
+
+        // check it is in run as well
+        onView(withId(R.id.back_button)).perform(click());
+
+        // duplicate of previous
+        onView(withId(R.id.routine_list_view)).check(matches(isDisplayed()));
+
+        onData(anything())
+                .inAdapterView(withId(R.id.routine))
+                .atPosition(0)
+                .onChildView(withId(R.id.routine_start_btn))
+                .perform(click());
+
+        onView(withId(R.id.fragment_active_routine)).check(matches(isDisplayed()));
+
+        lastPosition = getListViewSize(R.id.routine) - 1;
+
+        onData(anything())
+                .inAdapterView(withId(R.id.routine))
+                .atPosition(lastPosition)
+                .onChildView(withId(R.id.task_title))
+                .check(matches(withText("Test Task")));
+    }
+
+    private int getListViewSize(int listViewId) {
+        final int[] size = new int[1];
+        onView(withId(listViewId)).check((view, noViewFoundException) -> {
+            if (view instanceof ListView listView) {
+                size[0] = listView.getAdapter().getCount();
+            }
+        });
+        return size[0];
+    }
+}
